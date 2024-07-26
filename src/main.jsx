c2fdeaa617cc609353e9c3b64187eaa352d2c8ad
p34-pac <peterpaultolulope@gmail.com>
2024-07-26 23:48:13 +0100

    rem

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <SpeedInsights>
-        <Analytics>
-          <App />
-        </Analytics>
-      </SpeedInsights>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
