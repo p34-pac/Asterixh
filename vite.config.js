be9b754aaf2d8dbf061a4eecdf124da2580361ee
p34-pac <peterpaultolulope@gmail.com>
2024-07-26 23:29:24 +0100

    dev

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html")
+      }
+    }
+  }
 })
